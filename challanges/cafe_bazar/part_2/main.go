8c4b5b2bfd967261553b8b7f7fcb1730106a31cb
mohammadne <mohammadne@mail.ru>
2021-04-26 22:22:35 +0430

    feat: complete base ch2

--- challanges/cafe_bazar/part_2/main.go
+++ challanges/cafe_bazar/part_2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,7 +10,6 @@ import (
 )
 
 var (
-	fatalError        = "it should not happen, log."
 	errorInvalidInput = "Invalid input. Please enter 1 for more info."
 
 	help = "Select a number from shown menu and enter. For example 1 is for help."
@@ -33,27 +33,89 @@ type Road struct {
 	biDirectional bool
 }
 
-type models interface {
-	add(model interface{})
-	delete(id int)
-}
+// type models interface {
+// 	add(model interface{})
+// 	delete(id int)
+// }
 
 type Cities []City
 
-func (cities Cities) add(model interface{}) {
-	cities = append(cities, model.(City))
+func (cities Cities) add(model interface{}) Cities {
+	city := model.(City)
+
+	index := -1
+	for i, c := range cities {
+		if city.id == c.id {
+			index = i
+			break
+		}
+	}
+
+	if index >= 0 {
+		cities[index] = city
+	} else {
+		cities = append(cities, city)
+	}
+
+	return cities
 }
 
-func (cities Cities) delete(id int) {
+func (cities Cities) delete(id int) error {
+	contains := false
+
+	for i, city := range cities {
+		if city.id == id {
+			cities = append(cities[:i], cities[i+1:]...)
+			contains = true
+			break
+		}
+	}
+
+	if !contains {
+		return errors.New("")
+	}
+
+	return nil
 }
 
 type Roads []Road
 
-func (roads Roads) add(model interface{}) {
-	roads = append(roads, model.(Road))
+func (roads Roads) add(model interface{}) Roads {
+	road := model.(Road)
+
+	index := -1
+	for i, r := range roads {
+		if road.id == r.id {
+			index = i
+			break
+		}
+	}
+
+	if index >= 0 {
+		roads[index] = road
+	} else {
+		roads = append(roads, road)
+	}
+
+	return roads
 }
 
-func (roads Roads) delete(id int) {
+func (roads Roads) delete(id int) error {
+	contains := false
+
+	for i, road := range roads {
+		if road.id == id {
+			roads = append(roads[:i], roads[i+1:]...)
+			contains = true
+			break
+		}
+	}
+
+	if !contains {
+		return errors.New("")
+	}
+
+	return nil
 }
 
 func main() {
@@ -125,13 +187,35 @@ func processMainMenue(cities Cities, roads Roads) {
 			addRoad(roads, processMainMenueCallback)
 		}
 	case 3:
-		fmt.Println(help)
+		model := getModel()
+
+		strId := readeLine()
+		id, _ := strconv.Atoi(strId)
+
+		switch model {
+		case 1:
+			err := cities.delete(id)
+			if err != nil {
+				fmt.Printf("City:%d deleted!\n", id)
+			} else {
+				fmt.Printf("City with id %d not found!\n", id)
+			}
+
+		case 2:
+			err := roads.delete(id)
+			if err != nil {
+				fmt.Printf("Road:%d deleted!\n", id)
+			} else {
+				fmt.Printf("Road with id %d not found!\n", id)
+			}
+		}
+
+		processMainMenueCallback()
 	case 4:
-		fmt.Println(help)
+		showPath(cities, roads, processMainMenueCallback)
 	case 5:
 		os.Exit(0)
 	}
-
 }
 
 func multiInput(values []string) []string {
@@ -139,7 +223,7 @@ func multiInput(values []string) []string {
 
 	for _, value := range values {
 		fmt.Printf("%s=?\n", value)
-		values = append(values, readeLine())
+		output = append(output, readeLine())
 	}
 
 	return output
@@ -157,7 +241,7 @@ func addCity(cities Cities, menuCallback func()) {
 
 	cities.add(city)
 
-	fmt.Printf("City with id=%d added!", id)
+	fmt.Printf("City with id=%d added!\n", id)
 
 	action := printOptions(
 		"Select your next action",
@@ -187,7 +271,8 @@ func addRoad(roads Roads, menuCallback func()) {
 	)
 
 	id, _ := strconv.Atoi(input[0])
-	‫‪from‬‬, _ := strconv.Atoi(input[2])
+
+	from, _ := strconv.Atoi(input[2])
 	to, _ := strconv.Atoi(input[3])
 
 	through := make([]int, 0, 5)
@@ -201,23 +286,22 @@ func addRoad(roads Roads, menuCallback func()) {
 	speedLimit, _ := strconv.Atoi(input[5])
 	length, _ := strconv.Atoi(input[6])
 
-	‫‪biDirectional‬‬, _ := strconv.Atoi(input[7])
+	biDirectional, _ := strconv.Atoi(input[7])
 
 	road := Road{
-		id:      id,
-		name:    input[1],
-		from: from,
-		to:      to,
-		through: through,
-		speedLimit: speedLimit,
-		‫‪length‬‬: ‫‪length‬‬,
-		biDirectional: ‫‪biDirectional‬‬ == 1,
-		// from: ‫‪from‬‬,
+		id:            id,
+		name:          input[1],
+		from:          from,
+		to:            to,
+		through:       through,
+		speedLimit:    speedLimit,
+		length:        length,
+		biDirectional: biDirectional == 1,
 	}
 
 	roads.add(road)
 
-	fmt.Printf("Road with id=%d added!", id)
+	fmt.Printf("Road with id=%d added!\n", id)
 
 	action := printOptions(
 		"Select your next action",
@@ -230,4 +314,72 @@ func addRoad(roads Roads, menuCallback func()) {
 	case 2:
 		menuCallback()
 	}
+}
+
+func showPath(cities Cities, roads Roads, menuCallback func()) {
+	values := strings.Split(readeLine(), ":")
+	start, _ := strconv.Atoi(values[0])
+	end, _ := strconv.Atoi(values[1])
+
+	var sb strings.Builder
+
+	for _, road := range roads {
+		if road.biDirectional {
+			index := indexOfId(road.through, start)
+			if index != -1 {
+				index2 := indexOfId(road.through[index:], end)
+				if index2 != -1 || road.to == end {
+					sb.WriteString(pathOut(road, cities))
+				}
+			} else if road.to == start {
+				index2 := indexOfId(road.through, end)
+				if index2 != -1 {
+					sb.WriteString(pathOut(road, cities))
+				}
+			}
+		} else {
+			index := indexOfId(road.through, start)
+			if index != -1 {
+				index2 := indexOfId(road.through[index:], end)
+				if index2 != -1 || road.to == end {
+					sb.WriteString(pathOut(road, cities))
+				}
+			}
+		}
+	}
+
+	fmt.Println("Kashan:Qom via Road T-K: Takes 00:07:30")
+	menuCallback()
+}
+
+func pathOut(road Road, cities Cities) string {
+	citiesIdMap := make([]int, 0, len(cities))
+
+	startCity := cities[indexOfId(citiesIdMap, road.from)]
+	endCity := cities[indexOfId(citiesIdMap, road.from)]
+
+	time := dateTime(road.length, road.speedLimit)
+	return fmt.Sprintf("%s:%s via Road %s: Takes %s\n", startCity.name, endCity.name, road.name, time)
+}
+
+// index == -1 means it's not exist
+func indexOfId(slice []int, id int) int {
+	index := -1
+
+	for i, value := range slice {
+		if value == id {
+			index = i
+			break
+		}
+	}
+
+	return index
+}
+
+func dateTime(length int, speedLimit int) string {
+	days := (length / speedLimit) / 24
+	hours := length/speedLimit - days*24
+	mins := (float64(length%speedLimit) / float64(speedLimit)) * 60
+
+	return fmt.Sprintf("%02d:%02d:%02d", days, hours, int64(mins))
 }
