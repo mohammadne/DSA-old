a5625ff6c59385c3160af6d62712b0423946dbce
mohammadne <mohammadne@mail.ru>
2021-06-07 21:54:01 +0430

    feat: complete base

--- challanges/rabex/main.go
+++ challanges/rabex/main.go
@@ -12,6 +12,14 @@ const (
 	division       = "abab"
 )
 
+var worthOfAlphabets = map[byte]byte{
+	byte('a'): 1,
+	byte('b'): 2,
+	byte('c'): 3,
+	byte('d'): 4,
+	byte('e'): 5,
+}
+
 // Sentence has union type charactristics
 type Sentence struct {
 	verb         string
@@ -21,11 +29,34 @@ type Sentence struct {
 }
 
 func (s Sentence) process() int {
-	return 0
+	if noun := s.noun; len(noun) != 0 {
+		return processNoun(noun)
+	}
+
+	values := make([]int, 0, len(s.subSentences))
+
+	for index := 0; index < len(s.subSentences); index++ {
+		values = append(values, s.subSentences[index].process())
+	}
+
+	result := values[0]
+	for index := 1; index < len(values); index++ {
+		if s.verb == addition {
+			result += values[index]
+		} else if s.verb == subtraction {
+			result -= values[index]
+		} else if s.verb == multiplication {
+			result *= values[index]
+		} else if s.verb == division {
+			result /= values[index]
+		}
+	}
+
+	return result
 }
 
 func main() {
-	input := ""
+	input := "abcd abcd aabbc ab a c ccd dede cccd cd  "
 	processedInput := processString(input)
 	sentence := processTerms(processedInput)
 
@@ -71,17 +102,16 @@ func processTerms(terms []string) Sentence {
 		if verbsLength-index <= 1 {
 			sentence := processTerms(terms[verbsIndex[index]:])
 			subSentences = append(subSentences, sentence)
+		} else {
+			sentence := processTerms(terms[verbsIndex[index]:verbsIndex[index+1]])
+			subSentences = append(subSentences, sentence)
 		}
-
-		sentence := processTerms(terms[verbsIndex[index]:verbsIndex[index+1]])
-		subSentences = append(subSentences, sentence)
 	}
 
 	return Sentence{
 		subSentences: subSentences,
 		verb:         verb,
 	}
-
 }
 
 func isVerb(input string) bool {
@@ -89,4 +119,45 @@ func isVerb(input string) bool {
 		input == subtraction ||
 		input == multiplication ||
 		input == division
+}
+
+func processNoun(str string) int {
+	values := make([]int, 0, len(str))
+
+	repetition := 0
+	current := byte(0)
+
+	for index := 0; index < len(str); index++ {
+		if current == 0 {
+			current = str[index]
+			repetition++
+		} else if current != str[index] {
+			worth := worthOfAlphabets[current]
+			values = append(values, int(repetition)*int(worth))
+
+			current = byte(str[index])
+			repetition = 1
+		} else {
+			repetition++
+		}
+	}
+
+	worth := worthOfAlphabets[current]
+	values = append(values, int(repetition)*int(worth))
+
+	for index := 0; index < len(values); index++ {
+		newVal := values[index] % 5
+		values[index] = newVal * newVal
+	}
+
+	return sumIntSlice(values)
+}
+
+func sumIntSlice(slice []int) int {
+	sum := 0
+	for index := 0; index < len(slice); index++ {
+		sum += slice[index]
+	}
+
+	return sum
 }
